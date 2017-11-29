3ef58db04c5fa0d8a8ed0a1461caa7b3277c2fdc
Rodriguez <DanielRodriguez1@thomsonreuters.com>
2017-11-29 16:48:15 -0600

    fixed unit tests

--- imports/api/tasks/tasks.js
+++ imports/api/tasks/tasks.js
@@ -36,7 +36,7 @@ Meteor.methods({
         check(taskId, String);
 
         const task = Tasks.findOne(taskId);
-        if(task && task.owner !== Meteor.userId()) {
+        if(task && task.owner !== this.userId) {
             throw new Meteor.Error('not-authorized');
         }
 
